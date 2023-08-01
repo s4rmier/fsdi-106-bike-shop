ff545b68f2c3f14a4913707cf4d69c29877dc7a2
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2023-08-01 17:47:29 +0800

    nav UI -ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,29 @@
+export default function Navigation() {
+  return (
+    <nav>
+      <div className="nav--container flex-row justify-sb">
+        <div className="brand-container flex-row align">
+          <img src="/images/logo.svg" alt="" />
+          <h1>Urban Bikes</h1>
+        </div>
+        <ul className="nav-links align justify flex-row">
+          <li>Home</li>
+          <li>Catalog</li>
+          <li>About</li>
+        </ul>
+        <ul className="nav-social align flex-row">
+          <li>
+            <i className="fa-brands fa-instagram"></i>
+          </li>
+          <li>
+            <i className="fa-brands fa-twitter"></i>
+          </li>
+          <li>
+            <i className="fa-brands fa-yelp"></i>
+          </li>
+        </ul>
+        <i id="cart" className="fa-solid fa-cart-shopping flex-row align"></i>
+      </div>
+    </nav>
+  );
+}
