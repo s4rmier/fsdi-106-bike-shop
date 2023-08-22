701a0ee5215b6eebda71a291d2636e2c69cabd3d
Romnick Sarmiento <s4rmier@gmail.com>
2023-08-22 07:21:50 -0700

    hamburger menu

--- src/components/hamburgermenu.jsx
+++ src/components/hamburgermenu.jsx
@@ -29,6 +29,49 @@ export default function HamburgerMenu(props) {
             <li>Contact</li>
           </Link>
         </ul>
+        <div className="footer-contact flex-col">
+          <h2>Contact</h2>
+          <ul>
+            <li>
+              <span>ADD:</span> 2223 Alpine Blvd Alpine, CA, 91901
+            </li>
+            <li>
+              <span>TEL:</span> (619)722-4232
+            </li>
+            <li>
+              <span>E-MAIL:</span> contact@ubco.com
+            </li>
+            <li>https://ubco.com</li>
+          </ul>
+        </div>
+        <div className="footer-hours">
+          <h2>Hours</h2>
+          <ul>
+            <li>
+              <span>Mon - Fri: </span> 9AM-6PM
+            </li>
+            <li>
+              <span>Wednesday: </span> CLOSED
+            </li>
+            <li>
+              <span>Sat: </span> 6AM-4PM
+            </li>
+            <li>
+              <span>Sun: </span> 6AM-2PM
+            </li>
+            <li>
+              <span>Holidays: </span> 8AM - 1PM
+            </li>
+          </ul>
+        </div>
+        <div className="subscribe-label">
+          <h2>Subscribe</h2>
+          <p>Subscribe for latest news, events, and promotions!</p>
+        </div>
+        <div className="subscribe-form flex-row justify">
+          <input type="text" placeholder="Enter your e-mail" />
+          <button className="button">Submit</button>
+        </div>
       </div>
     </div>
   );
