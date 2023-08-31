2d37eac4263cee77a8067a2483e65737270f043d
Romnick Sarmiento <s4rmier@gmail.com>
2023-08-30 20:01:28 -0700

    final report -ok

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -14,12 +14,24 @@ export default function Admin() {
     stars: "",
   });
 
+  const [coupon, setCoupon] = useState({
+    code: "",
+    discount: "",
+  });
+
+  const [couponList, setCouponList] = useState([]);
+  async function loadCoupons() {
+    let service = new DataService();
+    let coupons = await service.getCoupons();
+    setCouponList(coupons);
+  }
+
   useEffect(function () {
     loadCatalog();
+    loadCoupons();
   }, []);
 
   const [catalogItems, setCatalogItems] = useState([]);
-
   async function loadCatalog() {
     let service = new DataService();
     let prods = await service.getProducts();
@@ -30,12 +42,34 @@ export default function Admin() {
     console.log(e.target.value);
     const val = e.target.value;
     const name = e.target.name;
-
-    // updating a state variable
-    // create a copy, modify and set
     let copy = { ...product };
     copy[name] = val;
     setProduct(copy);
+  }
+
+  function handleCouponChange(e) {
+    console.log(e.target.value);
+    const val = e.target.value;
+    const name = e.target.name;
+    let copy = { ...coupon };
+    copy[name] = val;
+    setCoupon(copy);
+  }
+
+  function removeProduct(id) {
+    let service = new DataService();
+    service.deleteProduct(id);
+
+    let filteredArray = catalogItems.filter((product) => product._id !== id);
+    setCatalogItems(filteredArray);
+  }
+
+  function removeCoupon(id) {
+    let service = new DataService();
+    service.deleteCoupon(id);
+
+    let filteredArray = couponList.filter((coupon) => coupon._id !== id);
+    setCouponList(filteredArray);
   }
 
   function saveProduct() {
@@ -48,6 +82,16 @@ export default function Admin() {
     clearForm();
   }
 
+  function saveCoupon() {
+    let couponCopy = { ...coupon };
+    couponCopy.discount = parseInt(couponCopy.discount);
+    let service = new DataService();
+    service.saveCoupon(couponCopy);
+    console.log(couponCopy);
+
+    clearForm();
+  }
+
   function clearForm() {
     setProduct({
       title: "",
@@ -55,68 +99,122 @@ export default function Admin() {
       image: "",
       price: "",
     });
+
+    setCoupon({
+      code: "",
+      discount: "",
+    });
   }
 
   return (
-    <main id="admin" className="container flex-col align">
+    <main id="admin" className="container flex-col justify">
       <h1>Administrator</h1>
+      <div className="flex-row justify">
+        <div className="flex-row justify">
+          <div className="form-container flex-col align">
+            <h3>Register New Product</h3>
+            <div>
+              <label>Title: </label>
+              <input
+                value={product.name}
+                onChange={handleInputChange}
+                name="name"
+                type="text"
+              />
+            </div>
+            <div>
+              <label>Category: </label>
+              <input
+                value={product.category}
+                onChange={handleInputChange}
+                name="category"
+                type="text"
+              />
+            </div>
+            <div>
+              <label>Image: </label>
+              <input
+                value={product.image}
+                onChange={handleInputChange}
+                name="image"
+                type="text"
+              />
+            </div>
+            <div>
+              <label>Price: </label>
+              <input
+                value={product.price}
+                onChange={handleInputChange}
+                name="price"
+                type="number"
+              />
+            </div>
+            <div>
+              <button onClick={saveProduct} className="button">
+                Save Product
+              </button>
+            </div>
+          </div>
+          <div className="coupon-form">
+            <h3>Register Coupon Code</h3>
+            <div>
+              <label>Code: </label>
+              <input
+                value={coupon.code}
+                onChange={handleCouponChange}
+                name="code"
+                type="text"
+              />
+            </div>
+            <div>
+              <label>Discount: </label>
+              <input
+                value={coupon.discount}
+                onChange={handleCouponChange}
+                name="discount"
+                type="text"
+              />
+            </div>
 
-      <div className="form-container flex-col justify">
-        <h3>Register New Product</h3>
-        <div>
-          <label>Title: </label>
-          <input
-            value={product.name}
-            onChange={handleInputChange}
-            name="name"
-            type="text"
-          />
+            <button onClick={saveCoupon} className="button">
+              Save Coupon
+            </button>
+          </div>
         </div>
-        <div>
-          <label>Category: </label>
-          <input
-            value={product.category}
-            onChange={handleInputChange}
-            name="category"
-            type="text"
-          />
+
+        <div className="product-list flex-row">
+          <ul className="flex-col">
+            <h2>Available Products</h2>
+            {catalogItems.map((product) => (
+              <li key={product._id} className="product-item flex-row align">
+                <img src={`/images/${product.image}`} alt="" />
+                <h2>{product.name}</h2>
+                <h3>${product.price}</h3>
+                <button
+                  onClick={() => removeProduct(product._id)}
+                  className="button"
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+          <ul className="coupon-list flex-col">
+            <h2>All Coupons</h2>
+            {couponList.map((coupon) => (
+              <li key={coupon.code} className="flex-row align">
+                <h2>Code: {coupon.code}</h2>
+                <h3>Discount: {coupon.discount}</h3>
+                <button
+                  onClick={() => removeCoupon(coupon._id)}
+                  className="button"
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div>
-          <label>Image: </label>
-          <input
-            value={product.image}
-            onChange={handleInputChange}
-            name="image"
-            type="text"
-          />
-        </div>
-        <div>
-          <label>Price: </label>
-          <input
-            value={product.price}
-            onChange={handleInputChange}
-            name="price"
-            type="number"
-          />
-        </div>
-        <div>
-          <button onClick={saveProduct} className="button">
-            Save Product
-          </button>
-        </div>
-      </div>
-      <div className="product-list">
-        <ul className="flex-col">
-          <h2>Available Products</h2>
-          {catalogItems.map((product) => (
-            <li key={product._id} className="product-item flex-row align">
-              <img src={`/images/${product.image}`} alt="" />
-              <h2>{product.name}</h2>
-              <h3>${product.price}</h3>
-              <button className="button">X</button>
-            </li>
-          ))}
-        </ul>
       </div>
     </main>
   );
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,73 +1,73 @@
 import axios from "axios";
 
-let catalog = [
-  {
-    name: "Trek Top Fuel 9x9 XX1 AXS PRO EVO PLUS",
-    make: "Trek",
-    price: 11499,
-    dcprice: 0,
-    image: "bike1.jpg",
-    isDiscounted: false,
-    sizes: "S/M/L",
-    stars: "4.8",
-    _id: "1",
-  },
-  {
-    name: "Slash 9.9 XX1 AXS COMP PLUS V5 6.2 XMS",
-    make: "Slash",
-    price: 12550,
-    dcprice: 11000,
-    image: "slash9.png",
-    isDiscounted: true,
-    sizes: "S/M/L/XL",
-    stars: "4.4",
-    _id: "2",
-  },
-  {
-    name: "Fuel Stratosphere EX 9.9 XX1 AXS 5mm",
-    make: "Fuel",
-    price: 10750,
-    dcprice: 0,
-    image: "fuelex.png",
-    isDiscounted: false,
-    sizes: "S/M/L",
-    stars: "4.2",
-    _id: "3",
-  },
-  {
-    name: "Specialized Lightweight S-Works RS",
-    make: "Specialized",
-    price: 12000,
-    dcprice: 11000,
-    image: "specevo.png",
-    isDiscounted: true,
-    sizes: "S/M/L/XL",
-    stars: "4.5",
-    _id: "4",
-  },
-  {
-    name: "Specialized Featherweight EPIC Evo Pro",
-    make: "Specialized",
-    price: 8900,
-    dcprice: 0,
-    image: "specevopro.png",
-    isDiscounted: false,
-    sizes: "S/M/L",
-    stars: "4.2",
-    _id: "5",
-  },
-  {
-    name: "Specialized EVO G1 XX1 Stumpjumper",
-    make: "Specialized",
-    price: 5000,
-    dcprice: 4500,
-    image: "stumpjumper.png",
-    isDiscounted: true,
-    sizes: "S/M/L/XL",
-    stars: "4.0",
-    _id: "6",
-  },
-];
+// let catalog = [
+//   {
+//     name: "Trek Top Fuel 9x9 XX1 AXS PRO EVO PLUS",
+//     make: "Trek",
+//     price: 11499,
+//     dcprice: 0,
+//     image: "bike1.jpg",
+//     isDiscounted: false,
+//     sizes: "S/M/L",
+//     stars: "4.8",
+//     _id: "1",
+//   },
+//   {
+//     name: "Slash 9.9 XX1 AXS COMP PLUS V5 6.2 XMS",
+//     make: "Slash",
+//     price: 12550,
+//     dcprice: 11000,
+//     image: "slash9.png",
+//     isDiscounted: true,
+//     sizes: "S/M/L/XL",
+//     stars: "4.4",
+//     _id: "2",
+//   },
+//   {
+//     name: "Fuel Stratosphere EX 9.9 XX1 AXS 5mm",
+//     make: "Fuel",
+//     price: 10750,
+//     dcprice: 0,
+//     image: "fuelex.png",
+//     isDiscounted: false,
+//     sizes: "S/M/L",
+//     stars: "4.2",
+//     _id: "3",
+//   },
+//   {
+//     name: "Specialized Lightweight S-Works RS",
+//     make: "Specialized",
+//     price: 12000,
+//     dcprice: 11000,
+//     image: "specevo.png",
+//     isDiscounted: true,
+//     sizes: "S/M/L/XL",
+//     stars: "4.5",
+//     _id: "4",
+//   },
+//   {
+//     name: "Specialized Featherweight EPIC Evo Pro",
+//     make: "Specialized",
+//     price: 8900,
+//     dcprice: 0,
+//     image: "specevopro.png",
+//     isDiscounted: false,
+//     sizes: "S/M/L",
+//     stars: "4.2",
+//     _id: "5",
+//   },
+//   {
+//     name: "Specialized EVO G1 XX1 Stumpjumper",
+//     make: "Specialized",
+//     price: 5000,
+//     dcprice: 4500,
+//     image: "stumpjumper.png",
+//     isDiscounted: true,
+//     sizes: "S/M/L/XL",
+//     stars: "4.0",
+//     _id: "6",
+//   },
+// ];
 
 class DataService {
   serverURL = "http://127.0.0.1:5000";
@@ -86,8 +86,32 @@ class DataService {
     return response.data;
   }
 
+  async getCoupons() {
+    const response = await axios.get(this.serverURL + "/api/coupons");
+    return response.data;
+  }
+
   async saveProduct(prod) {
     const response = await axios.post(this.serverURL + "/api/products", prod);
+    return response.data;
+  }
+
+  async deleteProduct(id) {
+    const response = await axios.delete(
+      this.serverURL + `/api/products/id/${id}`
+    );
+    return response.data;
+  }
+
+  async saveCoupon(coupon) {
+    const response = await axios.post(this.serverURL + "/api/coupons", coupon);
+    return response.data;
+  }
+
+  async deleteCoupon(id) {
+    const response = await axios.delete(
+      this.serverURL + `/api/coupons/id/${id}`
+    );
     return response.data;
   }
 }
